1169b23919c857bab59b2540341d5a83ce097f31
RamanDangol7 <ramandangol898@gmail.com>
2025-12-07 19:37:45 +0545

    Created a new login page, only fontend with css

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href ="style.css">
+</head>
+<body>
+    <div class="container">
+        <h3> Welcome To Our Webiste</h3>
+        
+    <label>
+       Username : 
+       <input type = "text" name="username">
+    </label>
+    <label> 
+        Password : 
+        <input type ="password" name = "password">
+    </label>
+
+    <label>
+    <input type ="submit" name ="login" value="login">
+    </label>
+
+         <p>Don't have account? <a href="Registration.php"> Sign in</a></p>
+    </div>
+
+</body>
+</html>
