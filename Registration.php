c364e4a13f4cec5200bc7a3a4774be823d2d3f92
RamanDangol7 <ramandangol898@gmail.com>
2025-12-09 22:41:57 +0545

    Registration page with sanitize,validate and prepared statement

--- Registration.php
+++ Registration.php
@@ -1,3 +1,76 @@
+<?php 
+//connecting database
+    require_once("Database/database.php");
+
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+            //sanitizing
+        $firstname  = triM($_POST['firstname']);
+        $lastname   = trim( $_POST['lastname']);
+        $gender     =$_POST['gender'];
+        $phone      = trim( $_POST['number']);
+        $email      = trim( $_POST['email']);
+        $username   = trim( $_POST['username']);
+        $password   = $_POST['password'];
+        $hashpw     = password_hash($password, PASSWORD_DEFAULT);
+
+        //Valildation
+        if(empty($firstname) || empty($lastname) || empty($gender) || empty($phone) || 
+            empty($email) || empty($username) || empty($password)){
+                
+                echo "All fileds should be filled.";
+                exit();
+            }
+            //email
+        if(!filter_var($email,FILTER_SANITIZE_EMAIL)){
+            echo "Enter the valid email.";
+            exit();
+        }
+        
+        //number
+         if(!preg_match("/^[0-9]{10}$/", $phone)){
+        echo "Invalid phone number.";
+        exit();
+         }
+        //checking the username exist or not
+
+        $check = $conn->prepare("SELECT * FROM users WHERE username=?");
+        $check->bind_param("s",$username);
+        $check->execute();
+        $result = $check->get_result();
+
+        if($result->num_rows > 0){
+            echo "username already exists";
+           exit();
+        }
+        
+        else{
+
+            //prepared_statement(Insertion)
+            $stmt = $conn->prepare("INSERT into users(first_name, last_name, gender, phone, email, username, password)
+                                                    VALUES(?,?,?,?,?,?,?) ");
+            $stmt->bind_param("sssssss",
+            $firstname,
+            $lastname,
+            $gender,
+            $phone,
+            $email,
+            $username,
+            $hashpw);
+            
+            if($stmt->execute()){
+                header("Location: index.php");
+                exit();
+            }
+        $stmt->close();
+        $check->close();
+        $conn->close();
+        }
+            
+    }
+    
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,9 +92,9 @@
         </label>
         <label>
             Gender
-            <input type="radio" name="gender" value="male">Male
-            <input type="radio" name="gender" value="female">Female
-            <input type="radio" name="gender" value="other">Others
+            <input type="radio" name="gender" value="Male">Male
+            <input type="radio" name="gender" value="Female">Female
+            <input type="radio" name="gender" value="Other">Other
         </label>
         <label>
             Mobile number
@@ -30,6 +103,10 @@
         <label>
             Email
             <input type="email" name="email">
+        </label>
+         <label>
+            Username
+            <input type="text" name="username">
         </label>
         <label>
             Password
