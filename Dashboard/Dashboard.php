c15188633d8edbddde703fd4331c58a8d378dfed
RamanDangol7 <ramandangol898@gmail.com>
2025-12-13 19:01:38 +0545

    link to css

--- Dashboard/Dashboard.php
+++ Dashboard/Dashboard.php
@@ -19,13 +19,16 @@ if($_SERVER['REQUEST_METHOD'] =="POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
+    <div class="container">
     <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>">
         <h3>Welcome <?php echo $_SESSION['firstname'];?></h3>
         <label>
             <input type ="submit" name="logout" value="logout">
         </label>
     </form>
+    </div>
 </body>
 </html>
